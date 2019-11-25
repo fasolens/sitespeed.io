2ffdcb12cfbb6970e9208ac9e44a279f297c80e2
Bartłomiej Zieliński <fasolens@gmail.com>
2019-11-25 23:25:07 +0100

    Delete not used files

--- test/prepostscripts/preSample.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function(context, commands) {
-  context.log.info('In pretask!!!');
-  await commands.navigate('https://www.sitespeed.io/');
-  context.taskData.loadedSitespeed = true;
-};
